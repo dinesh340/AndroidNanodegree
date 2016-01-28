a44dbf6d1b1aa209aa4096ca54363fbe4a896e8d
Dinesh Lokhande <Dinesh Lokhande>
2016-01-28 15:27:03 +0530

    My first commit

---
+++ app/src/main/java/com/lokhande/dinesh/applauncher/MainActivity.java
@@ -0,0 +1,57 @@
+package com.lokhande.dinesh.applauncher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This button will launch my Spotify streamer", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"This button will launch my Scores App",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"This button will launch my Library App",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"This button will launch my Build it bigger app",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"This button will launch my XYZ reader app",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"This button will launch my Capstone Project",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
